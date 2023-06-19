1be924f878150e91e333cbb6ffe0ad9800878517
drMuuguu <munkh-erdene@bitacademy.mn>
2023-06-19 19:43:55 +0800

    database ok

--- dompFront.js
+++ dompFront.js
@@ -1,26 +1,41 @@
+var select = document.querySelector("#input");
+function fromdatabase() {
+  // 1. query буцаах
+  // back end бэлэн болгох
+  // DOMParser.js ээс back endрүү хандаад consolelog рүү
+  // for each
+}
+
+for (var i = min; i <= max; i++) {
+  var opt = document.createElement("option");
+  opt.value = i;
+  opt.innerHTML = i;
+  select.appendChild(opt);
+}
+
 const button = document.querySelector("#button4");
 const searchbutton = document.querySelector("#input");
 button.onclick = (event) => {
-  fetch("http://localhost:3000?search=" + searchbutton.value, {
-    method: "POST",
+  fetch("http://localhost:3000/test?search=" + searchbutton.value, {
+    method: "get",
   }).then((x) => {
     x.json().then((data) => {
-      document.getElementById("name").innerHTML = "Name: " + data.name;
+      document.getElementById("name").innerHTML = "Name: " + data.latinName;
       console.log(data);
       document.getElementById("Definition").innerHTML =
-        "Definition: " + data.Definition;
+        "Definition: " + data.definition;
       document.getElementById("Synonyms").innerHTML =
-        "Synonyms: " + data.Synonyms;
+        "Synonyms: " + data.synonyms;
       document.getElementById("Description").innerHTML =
-        "Description:" + data.Description;
+        "Description:" + data.description;
       document.getElementById("PlantMaterialOfInterest").innerHTML =
-        "Plant Material Of Interest: " + data.PlantMaterialOfInterest;
+        "Plant Material Of Interest: " + data.plantMaterialOfInterest;
       document.getElementById("GeneralAppearance").innerHTML =
-        "General Appearance: " + data.GeneralAppearance;
+        "General Appearance: " + data.generalAppearance;
       document.getElementById("GeographicalDistribution").innerHTML =
-        "Geographical Distribution: " + data.GeographicalDistribution;
+        "Geographical Distribution: " + data.geographicalDistribution;
       document.getElementById("MedicinalUses").innerHTML =
-        "Medicinal Uses: " + data.MedicinalUses;
+        "Medicinal Uses: " + data.medicinalUses;
       document.getElementById("img1").src = data.img1;
       document.getElementById("img1").src = data.img2;
       document.getElementById("img1").src = data.img3;
